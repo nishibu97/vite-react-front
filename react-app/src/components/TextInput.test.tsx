fc3b3716592aca01375ee37f949e07bce8a563e1
nishibu97 <nishibu.work@outlook.jp>
2025-12-15 23:57:40 +0900

    test setup components

---
+++ react-app/src/components/TextInput.test.tsx
@@ -0,0 +1,16 @@
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import TextInput from "./TextInput";
+
+test("TextInput Component Test", async () => {
+  const user = userEvent.setup();
+  render(<TextInput />);
+
+  const textElement = screen.getByText(/Entered text:/);
+  expect(textElement).toBeInTheDocument();
+
+  const inputElement = screen.getByLabelText("text-input");
+  await user.type(inputElement, "Hello World");
+
+  expect(screen.getByText(/Entered text: Hello World/)).toBeInTheDocument();
+});
