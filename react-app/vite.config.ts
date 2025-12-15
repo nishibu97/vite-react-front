f80dee91e13b82ad5eb0b01cfe830e7ba05f26e6
nishibu97 <nishibu.work@outlook.jp>
2025-12-15 23:44:17 +0900

    test setup

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: "./vitest-setup.ts",
+  },
 });
