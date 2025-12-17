183f8bfcc6acda5f275d6cb79c40cadf88a1953b
nishibu97 <nishibu.work@outlook.jp>
2025-12-17 21:22:00 +0900

    prettier setting

--- react-app/src/components/TextInput.test.tsx
+++ react-app/src/components/TextInput.test.tsx
@@ -1,16 +1,16 @@
-import userEvent from "@testing-library/user-event";
-import { render, screen } from "@testing-library/react";
-import TextInput from "./TextInput";
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import TextInput from './TextInput';
 
-test("TextInput Component Test", async () => {
+test('TextInput Component Test', async () => {
   const user = userEvent.setup();
   render(<TextInput />);
 
   const textElement = screen.getByText(/Entered text:/);
   expect(textElement).toBeInTheDocument();
 
-  const inputElement = screen.getByLabelText("text-input");
-  await user.type(inputElement, "Hello World");
+  const inputElement = screen.getByLabelText('text-input');
+  await user.type(inputElement, 'Hello World');
 
   expect(screen.getByText(/Entered text: Hello World/)).toBeInTheDocument();
 });
