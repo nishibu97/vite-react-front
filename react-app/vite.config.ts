429af2a5844c39e72a7527fd18689c7840a268b9
nishibu97 <nishibu.work@outlook.jp>
2025-12-17 23:44:21 +0900

    shadcn/ui add

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,14 +1,20 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
-    environment: "happy-dom",
-    setupFiles: "./vitest-setup.ts",
+    environment: 'happy-dom',
+    setupFiles: './vitest-setup.ts',
   },
 });
