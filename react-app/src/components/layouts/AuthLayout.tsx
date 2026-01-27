dadcad69603b1af05309ca284f6956c42108cb8f
Kohei Nishibu <108510992+nishibu97@users.noreply.github.com>
2026-01-27 17:09:03 +0900

    Revert "shadcnuiを活用したログイン画面実装 (#3)"
    This reverts commit cdd015ea9ad477e59c8936c572bc9d85f2b09741.

--- react-app/src/components/layouts/AuthLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-export default function AuthLayout() {
-  return (
-    <div className="w-screen h-screen overflow-hidden">
-      <Outlet />
-    </div>
-  );
-}
